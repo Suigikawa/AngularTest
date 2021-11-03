cdc9be1ea6f43db6bd9895286fbd3fe948d0ffb0
Suigikawa <nghislain93@hotmail.fr>
2021-11-03 10:17:05 +0100

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
-import {ShippingComponent} from './shipping/shipping.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
